af261da8a60ac345b33ed5525f8697ceb3ea777f
Clark_Tsao <resj666@gmail.com>
2026-07-29 01:32:19 +0800

    Export bounded batch execution primitives

--- tsao_computation/execution/__init__.py
+++ tsao_computation/execution/__init__.py
@@ -1,3 +1,10 @@
+from .batch import BatchExecutionResult, run_plan_batch, run_plans
 from .runner import ExecutionRecord, run_plan
 
-__all__ = ["ExecutionRecord", "run_plan"]
+__all__ = [
+    "BatchExecutionResult",
+    "ExecutionRecord",
+    "run_plan",
+    "run_plan_batch",
+    "run_plans",
+]
